af469507aefc4581cdcc823ef1a3124da0425a53
MoinSul1 <76952036+MoinSul1@users.noreply.github.com>
2021-02-07 13:14:50 +0530

    4.Adding Bootstrap
    Adding bootstrap using command
    npm install bootstrap --save

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,21 +1,10 @@
-import { CoursesService } from './courses.service';
+
 import { Component } from '@angular/core'
 
 @Component({
     selector: 'courses',
-    template: `<h2>{{ getTitle() }}</h2>
-    
-                <h2 [textContent]="title"></h2>
-                
-                <ul>
-                <li *ngFor=" let course of courses">
-                {{ course }}
-                </li>
-
-                </ul>
-                <table>
-                <tr><td [attr.colspan]="colspan"></td></tr>
-                </table>
+    template: `
+    <button class="btn btn-primary">Save</button> 
 
                         `
 })
@@ -23,15 +12,7 @@ import { Component } from '@angular/core'
 
 export class CoursesComponent{
 
-    title = "List Of Courses ";
-    colspan=2;
+    
 
-    getTitle(){
-        return this.title;
-
-    }
-    courses;
-    constructor(service: CoursesService){
-        this.courses=service.getCourses();
-    }
+    
 }
