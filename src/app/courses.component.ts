4a988f46a50727ba16d2adb475bf11c7b71d2c9c
MoinSul1 <76952036+MoinSul1@users.noreply.github.com>
2021-02-07 15:46:34 +0530

    12.Custom pipes
    Created custom pipes here

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,26 +4,13 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'courses',
     template: `
-    {{course.title | uppercase | lowercase }} <br/>
-    {{course.students | number  }} <br/>
-    {{course.rating | number:'2.1-1'  }} <br/>
-    {{course.price | currency:'AUD': true:'3.2-2' }} <br/>
-    {{course.releaseDate | date: 'shortDate' }} <br/>
+    {{text | summary:10 }}
                         `
 })
 
 
 export class CoursesComponent{
-course = {
-    title: "The Complete Angular Course ",
-    rating: 4.9745,
-    students: 30123,
-    price: 190.95,
-    releaseDate: new Date(2021, 3, 1)
-
-
-}
-    
+text= `loren is  a bread banda and badnam fordkjsdkaslnfl adsaldjaisd adliajdliasjdsad sladjasldjl `   
 
     
 }
