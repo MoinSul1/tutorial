db46bc4e8aad2e788b7bff1fdbd9e9f560191dad
MoinSul1 <76952036+MoinSul1@users.noreply.github.com>
2021-02-07 11:46:44 +0530

    6.Directive
    used directive ngfor to display list

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `<h2>{{ getTitle() }}</h2>
+                <ul>
+                <li *ngFor=" let course of courses">
+                {{ course }}
+                </li>
+
+                </ul>
+
+                        `
 })
 
 
@@ -14,4 +22,5 @@ export class CoursesComponent{
         return this.title;
 
     }
+    courses = ["course1","course2","course3"];
 }
