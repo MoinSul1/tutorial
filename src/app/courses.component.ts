fca00ac7a5fd89878a47b3cc32ed10e9a70fed26
MoinSul1 <76952036+MoinSul1@users.noreply.github.com>
2021-02-07 14:00:42 +0530

    9.template variable using $event
    template variable using $event

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,14 +4,14 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'courses',
     template: `
-    <input (keyup.enter)="onKeyUp()"/>
+    <input (keyup.enter)="onKeyUp($event)"/>
                         `
 })
 
 
 export class CoursesComponent{
-onKeyUp(){
-    console.log("Enter was pressed");
+onKeyUp($event){
+    console.log($event.target.value);
 }
     
 
