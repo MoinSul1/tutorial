2cf74a6d798512c2677800c64c8f39da2c7f6993
MoinSul1 <76952036+MoinSul1@users.noreply.github.com>
2021-02-07 14:02:15 +0530

    9.template varibale using template variable
    template varibale using template variable

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,14 +4,14 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'courses',
     template: `
-    <input (keyup.enter)="onKeyUp($event)"/>
+    <input #email (keyup.enter)="onKeyUp(email.value)"/>
                         `
 })
 
 
 export class CoursesComponent{
-onKeyUp($event){
-    console.log($event.target.value);
+onKeyUp(email){
+    console.log(email);
 }
     
 
