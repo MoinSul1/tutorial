faa0e71f75fad496996abc82559d802a71cf9bb0
MoinSul1 <76952036+MoinSul1@users.noreply.github.com>
2021-02-07 11:10:13 +0530

    5.Templates
    Data binding and printing dynamically and string interpolation

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 })
 
 
 export class CoursesComponent{
 
+    title = "List Of Courses ";
+
+    getTitle(){
+        return this.title;
+
+    }
 }
