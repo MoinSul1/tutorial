f3670ad2f9b90417c7cd5c783a1fbecaad066ca1
MoinSul1 <moinsul.zversal@gmail.com>
2021-02-11 13:06:14 +0530

    11.Nested Form Groups
    nested form Groups

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -10,13 +10,14 @@ import { UsernameValidators } from './username.validators';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl('',
+    account: new FormGroup({
+      username: new FormControl('',
       Validators.required,
       UsernameValidators.shouldBeUnique),
-    password: new FormControl('',Validators.required)
+      password: new FormControl('',Validators.required)
 
-  }); 
-
+  }) 
+});
   login()
   {
     this.form.setErrors({
@@ -24,6 +25,6 @@ export class SignupFormComponent {
     })
   }
   get username(){
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
