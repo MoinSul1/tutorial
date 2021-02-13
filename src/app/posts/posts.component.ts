47bc6e9bfe1f7a5a151f741bf2fbfb1268bdaa13
MoinSul1 <moinsul.zversal@gmail.com>
2021-02-13 13:56:21 +0530

    4.creating data
    added a post request here to the api

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -10,15 +10,26 @@ import { Observable } from 'rxjs';
 })
 export class PostsComponent  {
   posts!: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient) { 
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor( private http: HttpClient) { 
+    http.get(this.url)
     .subscribe(response => {
     //console.log(response);
     this.posts = response;
   }) ;
 }
-  
+  createPost(input: HTMLInputElement){
+    let post = {title: input.value } ;
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe(response => {
+      post['id'] = response.id;
+      this.posts.splice(0,0,post);
+    });
+
+
+  }
  
 
 }
